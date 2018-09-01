b6e940f41627db19f1c87d7f239967c9e537eeca
Dane Jordan <drjordy66@hotmail.com>
2018-09-01 05:31:47 -0700

    Update 0003_Longest_Substring_Without_Repeating_Characters.py

--- Solutions/0003_Longest_Substring_Without_Repeating_Characters.py
+++ Solutions/0003_Longest_Substring_Without_Repeating_Characters.py
@@ -4,28 +4,6 @@ class Solution:
         :type s: str
         :rtype: int
         """
-        # # Original solution (time limit exceeded)
-        # x = [str(i) for i in str(s)]
-        # max_length = 0
-        # rep_index = 0
-        # for i in range(len(x)):
-        #     substr = []
-        #     for j in x[i + rep_index:]:
-        #         if j not in substr:
-        #             substr.append(j)
-        #         else:
-        #             rep_index += substr.index(j)
-        #             break
-        #     length = len(substr)
-        #     max_length = max(max_length, length)
-        #     if substr == []:
-        #         break
-        #     elif len(substr) == len(x[i + rep_index:]):
-        #         break
-        #     else:
-        #         pass
-        # return max_length
-
         x = [str(i) for i in str(s)]
         max_length = 0
         substr = []
